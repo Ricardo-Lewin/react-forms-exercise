60ae59c65519301d6fabcd57bb1fcfc332d7f07a
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-08-29 00:20:49 -0400

    completed part two

--- todo-app/src/App.test.js
+++ todo-app/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from "@testing-library/react";
 import App from './App';
 
 it('renders without crashing', function() {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("matches snapshot", function() {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
 });
